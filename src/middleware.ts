eea55c3bc2c0b9f8afa4064f284f139346ec74b8
Alukesh <akenzhebaev422@gmail.com>
2024-04-07 23:15:50 +0600

    setup convex muttations and querys in project

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,8 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes: ['/'],
 });
- 
+  
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
